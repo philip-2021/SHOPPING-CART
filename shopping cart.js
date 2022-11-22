92b9b6f70366c2f0419c210e3ae6d15afca7c2d2
Philip Ondieki <92627995+philip-2021@users.noreply.github.com>
2022-11-22 15:24:48 +0300

    Add files via upload

---
+++ shopping cart.js
@@ -0,0 +1,50 @@
+export const products = [
+  {
+    id: 1,
+    title: "Bed king",
+    price: "ksh 24000",
+    imgUrl: "./assets/images/product-1.jpeg",
+  },
+  {
+    id: 2,
+    title: "Sun Bed",
+    price: "ksh 34000",
+    imgUrl: "./assets/images/product-2.jpeg",
+  },
+  {
+    id: 3,
+    title: "Family Bed",
+    price: "ksh 120000",
+    imgUrl: "./assets/images/product-3.jpeg",
+  },
+  {
+    id: 4,
+    title: "wood Bed",
+    price: "ksh 55000",
+    imgUrl: "./assets/images/product-4.jpeg",
+  },
+  {
+    id: 5,
+    title: "Kitchen Cabinet",
+    price: "ksh 45000",
+    imgUrl: "./assets/images/product-5.jpeg",
+  },
+  {
+    id: 6,
+    title: "Leisure Bed",
+    price: "ksh 70000",
+    imgUrl: "./assets/images/product-6.jpeg",
+  },
+  {
+    id: 7,
+    title: "Super Sofaset",
+    price: "ksh 90000",
+    imgUrl: "./assets/images/product-7.jpeg",
+  },
+  {
+    id: 8,
+    title: "Bespoke table",
+    price: "ksh 20000",
+    imgUrl: "./assets/images/product-8.jpeg",
+  },
+];
